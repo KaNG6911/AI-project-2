8820ad698ca8976bd67c0ec510437019d1c872f8
25LP5951 25LP5951 <gna.k6911@gmail.com>
2026-03-11 10:00:14 +0800

    dcbwbebwiubdeiw

--- nextjs-prisma/next.config.ts
+++ nextjs-prisma/next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  }
+  // env: {
+  //   DATABASE_URL: process.env.DATABASE_URL,
+  //   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  //   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  // }
 };
 
 export default nextConfig;
